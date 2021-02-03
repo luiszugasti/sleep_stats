759aebefc6898789eb51bf9dc74cf8d8c7a85080
Kavin <kavinanand77@gmail.com>
2021-02-03 13:41:23 -0800

    time in bed

--- loadData.py
+++ loadData.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 data = pd.read_csv("data/sleepdata.csv",delimiter=";")
 data = data.iloc[:, : 4]
+for elm in data["Time in bed"]:
+    s = elm.split(":")
+    elm = int(s[0]) * 60 + int(s[1])
+    print(elm)
+print(data["Time in bed"])
