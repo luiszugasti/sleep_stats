e9ce48339967dacb79d23481dcbb5874eee1d40f
Luis <luis.zugasti@outlook.com>
2021-02-05 12:46:29 -0500

    format some files and play with algorithms
    Played around with the tutorial from
    https://github.com/alan-turing-institute/sktime/blob/master/examples/01_forecasting.ipynb
    to get a true feel for forecasting
    
    Attempt with sleep data is next

--- loadData.py
+++ loadData.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 
+
 def dataLoader():
-    df = pd.read_csv("data/sleepdata.csv") # may need to add os.getcwd() if on windows machine
+    df = pd.read_csv("data/sleepdata.csv")  # may need to add os.getcwd() if on windows machine
     # Drop extraneous columns not used in analysis
     df = df.drop('Heart rate', 1)
     df = df.drop('Activity (steps)', 1)
@@ -14,20 +15,19 @@ def dataLoader():
     df['Sleep quality'] = df['Sleep quality'].apply(truncate_percentage_to_range)
     df['Time in bed'] = df['Time in bed'].apply(time_to_minutes)
     df['Start'] = df['Start'].apply(extract_date)
-    df = df.rename(columns={'Start':'Date'})    
+    df = df.rename(columns={'Start': 'Date'})
 
     # Obtain user data
-    user_df = pd.read_csv("test/test_inputs/sample_7_days.csv") # may need to add os.getcwd() if on windows machine
+    user_df = pd.read_csv("test/test_inputs/sample_7_days.csv")  # may need to add os.getcwd() if on windows machine
     user_df["Time in bed"] = 60 * user_df["Time in bed"]
 
     # Append user data to test
     result = df.append(user_df)
     sleepArray = result.iloc[:, 0:2]
-    timeInBedArray = result[['Date','Time in bed']]
+    timeInBedArray = result[['Date', 'Time in bed']]
     print(sleepArray)
     print(timeInBedArray)
     return sleepArray, timeInBedArray
-
 
 
 # Helper functions for modifying the data to fit the correct format
@@ -36,14 +36,17 @@ def truncate_percentage_to_range(x):
     x = int(x) / 10.0
     return x
 
+
 def time_to_minutes(x):
     s = x.split(":")
     s = int(s[0]) * 60 + int(s[1])
     return s
 
+
 def extract_date(x):
     s = x.split(" ")
     return s[0]
+
 
 if __name__ == '__main__':
     dataLoader()
